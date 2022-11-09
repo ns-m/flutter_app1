f58a13166fb678177bcd03ead3745073b83d1a47
KMV <9117479@gmail.com>
2022-11-09 15:44:18 +0300

    finish RPS

--- lib/rock_paper_scissors.dart
+++ lib/rock_paper_scissors.dart
@@ -1,7 +1,38 @@
 import 'dart:io';
+import 'dart:math';
+
+enum Move { rock, paper, scrissors }
 
 void main() {
-  print('Rock, paper or scrissors? (r/p/s)');
-  final input = stdin.readLineSync();
-  print(input);
+  while (true) {
+    stdout.write('Rock, paper or scrissors? (r/p/s) ');
+    final input = stdin.readLineSync();
+    if (input == 'r' || input == 'p' || input == 's') {
+      var playerMove;
+      if (input == 'r') {
+        playerMove = Move.rock;
+      } else if (input == 'p') {
+        playerMove = Move.paper;
+      } else {
+        playerMove = Move.scrissors;
+      }
+      final aiMove = Move.values[Random().nextInt(3)];
+      print('You played - $playerMove');
+      print('AI played - $aiMove');
+      if (playerMove == aiMove) {
+        print('It`s a draw!');
+      } else if (playerMove == Move.rock && aiMove == Move.scrissors ||
+          playerMove == Move.paper && aiMove == Move.rock ||
+          playerMove == Move.scrissors && aiMove == Move.paper) {
+        print('You win!!!');
+      } else {
+        print('You looser((');
+      }
+    } else if (input == 'q') {
+      print('Bye bye!');
+      break;
+    } else {
+      print('Invalid input');
+    }
+  }
 }
