2898bce91271d34c0fc799e561f5060ef02a414f
KMV <9117479@gmail.com>
2022-11-10 17:49:33 +0300

    start pizza

---
+++ lib/pizza_prices.dart
@@ -0,0 +1,14 @@
+
+void main() {
+  const pizzaPrices = {
+    'margherita' : 5.5,
+    'pepperoni' : 7.5,
+    'vegetarian' : 6.5,
+  };
+  const order = [];
+  while(true){
+    stdout.write('margherita, pepperoni or vegetarian? (m/p/v) ');
+    final inputOrderItem = stdin.readLineSync();
+  }
+  
+}
