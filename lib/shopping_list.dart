c480a9fa661695990465fb6aca6e57c358582814
KMV <9117479@gmail.com>
2022-11-14 16:45:26 +0300

    fin

--- lib/shopping_list.dart
+++ lib/shopping_list.dart
@@ -1,1 +1,17 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  const bananas = 5;
+  const apples = 6;
+  const grains = {
+    'pasta': '500gr',
+    'rice': '1kg',
+  };
+  const addGrains = true;
+
+  var shoppingList = {
+    if (bananas > 0) 'bananas': bananas,
+    if (apples > 0) 'apples': apples,
+    if (addGrains) ...grains,
+  };
+
+  print(shoppingList);
+}
