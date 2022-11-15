6d13119c24efdfd89fe3689e0f63ef9bf629016b
KMV <9117479@gmail.com>
2022-11-15 12:21:18 +0300

    data analisys

--- lib/test.dart
+++ lib/test.dart
@@ -1,24 +1,33 @@
 void main(List<String> args) {
-  const addBlue = false;
-  const addRed = true;
-  const addExtraColors = true;
+  // const addBlue = false;
+  // const addRed = true;
+  // const addExtraColors = true;
 
-  final colors = [
-    'grey',
-    'brown',
-    if (addBlue) 'blue',
-    if (addRed) 'red',
-    if (addExtraColors) ...['yellow', 'green'],
-  ];
-  print(colors);
+  // final colors = [
+  //   'grey',
+  //   'brown',
+  //   if (addBlue) 'blue',
+  //   if (addRed) 'red',
+  //   if (addExtraColors) ...['yellow', 'green'],
+  // ];
+  // print(colors);
 
-  const ratings = [4.0, 3.5, 4.5];
-  final restaurant = {
-    'name': 'Pizza Mario',
-    'cuisine': 'Italian',
-    if (ratings.length > 3) ...{
-      'ratings': ratings,
-      'isPopular': true,
-    }
-  };
+  // const ratings = [4.0, 3.5, 4.5];
+  // final restaurant = {
+  //   'name': 'Pizza Mario',
+  //   'cuisine': 'Italian',
+  //   if (ratings.length > 3) ...{
+  //     'ratings': ratings,
+  //     'isPopular': true,
+  //   }
+  // };
+
+  final list = [1, 2, 3];
+  final copyList = list;
+  copyList[0] = 0;
+  final copyList2 = [...list];
+  copyList2[2] = 4;
+  print('list : $list');
+  print('copyList : $copyList');
+  print('copyList2 : $copyList2');
 }
