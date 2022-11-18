50fbc8670a69ec8b332cd387a801b9ff2eaa5e98
KMV <9117479@gmail.com>
2022-11-18 10:31:13 +0300

    convert types

---
+++ lib/convert.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  var a, b, c;
+  a = 40;
+  b = '1';
+  c = a + int.parse(b);
+  print('$a + $b = $c');
+
+  var d, e, f;
+  d = 40;
+  e = '0.1';
+  f = d + double.parse(e);
+  print('$d + $e = $f');
+
+  var g, h, i;
+  g = 40;
+  h = '1';
+  i = g.toString() + h;
+  print('$g concat $h will be $i');
+}
