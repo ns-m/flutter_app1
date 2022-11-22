fcfa1a56534d93d17b7f3de5a7a27954a58027e6
KMV <9117479@gmail.com>
2022-11-22 10:26:02 +0300

    fizz buzz

---
+++ lib/fizzbuzz.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  int num = 1;
+  while (num <= 100) {
+    if (num % 3 == 0 && num % 5 == 0) {
+      print('$num. Fizzbuzz!');
+    } else if (num % 3 == 0) {
+      print('$num. Fizz!');
+    } else if (num % 5 == 0) {
+      print('$num. Buzz!');
+    } else {
+      print('$num.');
+    }
+    num++;
+  }
+}
