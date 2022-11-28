d74f4f657ffe8c06fa990a7805146d5eca0d9758
KMV <9117479@gmail.com>
2022-11-28 17:54:49 +0300

    MathQuestion 2

--- lib/math_quiz.dart
+++ lib/math_quiz.dart
@@ -9,5 +9,13 @@ class MathQuestion {
 }
 
 void main(List<String> args) {
-  MathQuestion question1 = MathQuestion('3 + 5', 8);
+  List<MathQuestion> questions = [
+    MathQuestion('3 + 5', 8.0),
+    MathQuestion('10 - 7', 3.0),
+    MathQuestion('100 * 9', 900.0)
+  ];
+
+  for (MathQuestion mathQuestion in questions) {
+    //double userAnswer = 00;
+  }
 }
