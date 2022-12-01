775bbc6818108652d308a83fb669801a4ee735e6
KMV <9117479@gmail.com>
2022-12-01 16:41:26 +0300

    01122022

--- lib/dart_get_up_to_seed.dart
+++ lib/dart_get_up_to_seed.dart
@@ -1,4 +1,30 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  final names = ['John', 'Lee', 'Kate'];
+  final nameLength = names.map((name) => name.length);
+  // nameLength[0]; it`s mistake
+  final nameLength2 = names.map((name) => name.length).toList();
+  print(nameLength2[0]);
+
+  final nameLengthFiltered = names.where((name) => name.length >= 4).toList();
+  print(nameLengthFiltered);
+
+  for (int i = 0; i < nameLengthFiltered.length; i++) {
+    print(nameLengthFiltered[i]);
+  }
+
+  for (final name in nameLengthFiltered) {
+    print(name);
+  }
+  print('--------');
+  nameLengthFiltered.forEach((name) => print(name));
+  nameLengthFiltered.forEach(print);
+
+  print('--------');
+  myData();
+
+  print('////////');
+  myEnum();
+}
 
 void optionalPositionParams(int x, double y, [String? greeting]) {
   optionalPositionParams(5, 3.5);
@@ -16,4 +42,62 @@ void namedRequireParams(
   required String greeting,
 }) {
   namedRequireParams(77, x: 6, y: 5.5, greeting: 'hello');
+
+  List<int> myList = [1, 2, 3];
+
+  final myList2 = [1, 2, 3];
+  final myList3 = [1, 2, 3, 'a'];
+
+  Map<String, dynamic> myMap = {
+    'name': 'John Doe',
+    'age': 24,
+    'registered': true,
+  };
+
+  final myMapName = myMap['name'];
+}
+
+void myData() {
+  Set<int> mySet = {1, 2, 3, 4, 2};
+  print(mySet.length); // =4
+
+  bool isSignedIn = true;
+
+  <String>[
+    'This is a fake content',
+    if (isSignedIn) 'Sign out' else 'Sign In',
+  ];
+
+  final x = <String>[
+    for (int i = 0; i < 6; i++) i.toString(),
+    for (final number in [1, 2, 3]) number.toString(),
+  ];
+  print(x);
+
+  final list1 = ['hello', 'there'];
+  final list2 = ['what', 'up'];
+  final y = <String>[
+    ...list1,
+    ...list2,
+  ];
+  print(y);
+}
+
+enum AccountType { free, premium, vip }
+
+void myEnum() {
+  final userAccountType = AccountType.premium;
+  print(userAccountType.index);
+  print(AccountType.values[1]);
+
+  switch (userAccountType) {
+    case AccountType.free:
+      print('0 usd');
+      break;
+    case AccountType.premium:
+      print('20 usd');
+      break;
+    default:
+      break;
+  }
 }
