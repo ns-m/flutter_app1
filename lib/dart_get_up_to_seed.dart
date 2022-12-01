82afd028df6242ecf9380b55f5c0851d1bfc1435
KMV <9117479@gmail.com>
2022-12-01 12:42:19 +0300

    Params in func

--- lib/dart_get_up_to_seed.dart
+++ lib/dart_get_up_to_seed.dart
@@ -1,1 +1,19 @@
 void main(List<String> args) {}
+
+void optionalPositionParams(int x, double y, [String? greeting]) {
+  optionalPositionParams(5, 3.5);
+  optionalPositionParams(5, 3.5, 'hi');
+}
+
+void namedOptionalParams({int? x, double? y, String greeting = 'hi'}) {
+  namedOptionalParams(x: 5, greeting: 'bye');
+}
+
+void namedRequireParams(
+  int positional, {
+  required int x,
+  required double y,
+  required String greeting,
+}) {
+  namedRequireParams(77, x: 6, y: 5.5, greeting: 'hello');
+}
