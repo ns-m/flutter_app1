81b82a559101efabe4d52af60b26f8a15a71953f
KMV <9117479@gmail.com>
2022-12-06 17:51:47 +0300

    06122022

--- lib/lazy_load_dart.dart
+++ lib/lazy_load_dart.dart
@@ -1,7 +1,7 @@
-void main(List<String> args) {
-  var x = int.tryParse('dfs');
-  print(x);
-}
+// void main(List<String> args) {
+//   var x = int.tryParse('dfs');
+//   print(x);
+// }
 
 /*
 7 6 5 4 3 2 1 0    - powers of the digit 2
@@ -19,6 +19,43 @@ void main(List<String> args) {
 0 0 0 0 1 0 1 0 = 10 ((2 ^ 3 = 8) + (2 ^ 1 = 2))
 */
 
+import 'dart:io';
+
 int a = 0;
 
 int sumTest(int x1, int y1) => x1 + y1;
+
+int timesTwo(int x) {
+  return x * 2;
+}
+
+double someName(double x) {
+  return x * x;
+}
+
+int timesFour(int x) => timesTwo(timesTwo(x));
+
+double runTwice(double x, double Function(double) f) {
+  for (int i = 0; i < 2; i++) {
+    x = f(x);
+  }
+  return x;
+}
+
+void main(List<String> args) {
+  print('4 times two is ${timesTwo(4)}');
+  print('4 times four is ${timesFour(4)}');
+  final result = runTwice(4, (x) => x - x);
+  print(result);
+
+  final mySum = (int x, int y) {
+    return x + y;
+  };
+
+  print('Input number: ');
+  final myData = stdin.readLineSync() ?? '';
+  final myNum = int.tryParse(myData);
+
+  print(myData);
+  print(myNum);
+}
