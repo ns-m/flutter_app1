5938b61b853e7169ed328cb24635fc97548a4fa9
KMV <9117479@gmail.com>
2022-12-07 23:06:57 +0300

    07122022

--- lib/lazy_load_dart.dart
+++ lib/lazy_load_dart.dart
@@ -21,41 +21,58 @@
 
 import 'dart:io';
 
-int a = 0;
+// int a = 0;
 
-int sumTest(int x1, int y1) => x1 + y1;
+// int sumTest(int x1, int y1) => x1 + y1;
 
-int timesTwo(int x) {
-  return x * 2;
-}
+// int timesTwo(int x) {
+//   return x * 2;
+// }
 
-double someName(double x) {
-  return x * x;
-}
+// double someName(double x) {
+//   return x * x;
+// }
 
-int timesFour(int x) => timesTwo(timesTwo(x));
+// int timesFour(int x) => timesTwo(timesTwo(x));
 
-double runTwice(double x, double Function(double) f) {
-  for (int i = 0; i < 2; i++) {
-    x = f(x);
-  }
-  return x;
-}
+// double runTwice(double x, double Function(double) f) {
+//   for (int i = 0; i < 2; i++) {
+//     x = f(x);
+//   }
+//   return x;
+// }
+
+// void main(List<String> args) {
+//   print('4 times two is ${timesTwo(4)}');
+//   print('4 times four is ${timesFour(4)}');
+//   final result = runTwice(4, (x) => x - x);
+//   print(result);
+
+//   final mySum = (int x, int y) {
+//     return x + y;
+//   };
+
+//   print('Input number: ');
+//   final myData = stdin.readLineSync() ?? '';
+//   final myNum = int.tryParse(myData);
+
+//   print(myData);
+//   print(myNum);
+// }
 
 void main(List<String> args) {
-  print('4 times two is ${timesTwo(4)}');
-  print('4 times four is ${timesFour(4)}');
-  final result = runTwice(4, (x) => x - x);
-  print(result);
-
-  final mySum = (int x, int y) {
-    return x + y;
-  };
-
-  print('Input number: ');
-  final myData = stdin.readLineSync() ?? '';
-  final myNum = int.tryParse(myData);
-
-  print(myData);
-  print(myNum);
+  final listInt = [
+    1,
+    2,
+    3,
+    56,
+    77,
+    765,
+    4323,
+    777,
+    1234,
+    36646,
+    5555,
+    43679,
+  ];
 }
