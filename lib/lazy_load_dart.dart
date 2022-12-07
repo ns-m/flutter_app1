142d239b6d7fe790dd4e8a11fdbbbd97d3b6236e
KMV <9117479@gmail.com>
2022-12-07 14:14:30 +0300

    07122022

--- lib/lazy_load_dart.dart
+++ lib/lazy_load_dart.dart
@@ -19,43 +19,86 @@
 0 0 0 0 1 0 1 0 = 10 ((2 ^ 3 = 8) + (2 ^ 1 = 2))
 */
 
-import 'dart:io';
+// import 'dart:io';
 
-int a = 0;
+// int a = 0;
 
-int sumTest(int x1, int y1) => x1 + y1;
+// int sumTest(int x1, int y1) => x1 + y1;
 
-int timesTwo(int x) {
-  return x * 2;
+// int timesTwo(int x) {
+//   return x * 2;
+// }
+
+// double someName(double x) {
+//   return x * x;
+// }
+
+// int timesFour(int x) => timesTwo(timesTwo(x));
+
+// double runTwice(double x, double Function(double) f) {
+//   for (int i = 0; i < 2; i++) {
+//     x = f(x);
+//   }
+//   return x;
+// }
+
+//void main(List<String> args) {
+// print('4 times two is ${timesTwo(4)}');
+// print('4 times four is ${timesFour(4)}');
+// final result = runTwice(4, (x) => x - x);
+// print(result);
+
+// final mySum = (int x, int y) {
+//   return x + y;
+// };
+
+// print('Input number: ');
+// final myData = stdin.readLineSync() ?? '';
+// final myNum = int.tryParse(myData);
+
+// print(myData);
+// print(myNum);
+//}
+int sum(int x, int y, [int z = 0, int q = 90]) {
+  return x + y + z + q;
 }
 
-double someName(double x) {
-  return x * x;
+String makeUser({
+  String name = '',
+  String surName = '',
+  int age = 0,
+  passport = '',
+}) {
+  return '$name $surName $age $passport';
 }
 
-int timesFour(int x) => timesTwo(timesTwo(x));
+String makeUser2({
+  required String name,
+  required String surName,
+  required int age,
+  passport = '',
+}) {
+  return '$name $surName $age $passport';
+}
 
-double runTwice(double x, double Function(double) f) {
-  for (int i = 0; i < 2; i++) {
-    x = f(x);
-  }
-  return x;
+class MakeUser3 {
+  final String name;
+  final String surName;
+  final int age;
+  final String passport;
+
+  MakeUser3({
+    required this.name,
+    required this.surName,
+    required this.age,
+    this.passport = '',
+  });
 }
 
 void main(List<String> args) {
-  print('4 times two is ${timesTwo(4)}');
-  print('4 times four is ${timesFour(4)}');
-  final result = runTwice(4, (x) => x - x);
-  print(result);
-
-  final mySum = (int x, int y) {
-    return x + y;
-  };
-
-  print('Input number: ');
-  final myData = stdin.readLineSync() ?? '';
-  final myNum = int.tryParse(myData);
-
-  print(myData);
-  print(myNum);
+  final myUserData = makeUser(name: 'Joe', surName: 'Do', age: 20);
+  print(myUserData);
+  final myUserData2 = makeUser2(name: 'name', surName: 'surName', age: 18);
+  print(myUserData2);
+  MakeUser3(age: 23, surName: 'Lee', name: 'Bruce');
 }
